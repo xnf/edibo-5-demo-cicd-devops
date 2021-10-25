c3ac266dc6b42c8194376b7cadceb15b30c3ddd6
Edgar Zagorskis <edgars.zagorskis@gmail.com>
2021-10-25 21:14:34 +0300

    #3 - add test

---
+++ __tests__/script.test.ts
@@ -0,0 +1,13 @@
+import {dividivide, rezireizina} from "../src/script";
+
+it('should return 4 when i multiply 2 and 2', ()=>{
+   expect(rezireizina(2, 2)).toBe(4);
+});
+
+it('should return 5 when 10 divided by 2', ()=>{
+   expect(dividivide(10, 2)).toBe(5);
+});
+
+it('should return null when divided by zero', ()=>{
+   expect(dividivide(10,0)).toBe(Infinity);
+})
