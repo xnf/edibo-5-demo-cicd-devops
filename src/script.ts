7c0fbbc8a67b8229548aef7bd475637d9bdacba6
Edgar Zagorskis <edgars.zagorskis@gmail.com>
2021-10-25 21:25:04 +0300

    #4 - Jā, viss okej, !!!

--- src/script.ts
+++ src/script.ts
@@ -1,4 +1,4 @@
-export const rezireizina = (a, b) => a * b;
+export const rezireizina = (a, b) => a * b * b;
 export const dividivide = (a, b) => b === 0 ? Infinity : a / b;
 
 console.log(rezireizina(4, 5));
